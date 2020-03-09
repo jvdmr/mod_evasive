288830ec455b3aeaa82cb3f8cd6a9a0a968a0938
Gatis Tomsons <gatis.tomsons@eazybi.com>
2020-03-09 10:46:26 +0200

    create the dir before move

--- debian-build.sh
+++ debian-build.sh
@@ -1,3 +1,4 @@
 cd dist
-cp /usr/lib/apache2/modules/mod_evasive.so libapache2-mod-evasive/usr/lib/apache2/modules/mod_evasive.so
+mkdir -p libapache2-mod-evasive/usr/lib/apache2/modules
+cp /usr/lib/apache2/modules/mod_evasive.so libapache2-mod-evasive/usr/lib/apache2/modules
 dpkg-deb --build libapache2-mod-evasive
