d37f0e28e59967e1d2103ad6ccab308ca4388def
Joris Vandermeersch <joris@vdmr.be>
2024-07-28 14:54:57 +0200

    improve testing

--- test/00_regular_config/test.pl
+++ test/00_regular_config/test.pl
@@ -18,16 +18,18 @@ sub request {
   print $SOCKET "GET $uri?$i HTTP/1.1\r\n";
   print $SOCKET "Host: $address\r\n\r\n";
   $response = <$SOCKET>;
-  print $address . ": " . $response;
+	chomp $response;
+  print "$i - $address: $response\n";
   close($SOCKET);
 }
 
-for(0..100) {
-	request "a.site", "/", $_;
+for(0..900) {
+	request "a.site", "/j_spring_security_check", $_;
 }
 for(0..50) {
 	request "b.site", "/", $_;
 }
 for(0..100) {
-  request "a.site", "/whitelisted-uri", $_;
+	request "a.site", "/whitelisted-uri", $_;
 }
+print "done.\n"
