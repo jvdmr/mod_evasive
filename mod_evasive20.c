aba61d41fcbe953f2abe2844fa3209707d60112c
Joris Vandermeersch <joris@vdmr.be>
2024-07-28 23:02:44 +0200

    Bugfix segfault issue on NULL config for apache2.0 and windows apache2.4

--- mod_evasive20.c
+++ mod_evasive20.c
@@ -322,11 +322,13 @@ int is_whitelisted(const char *ip, evasive_config *cfg) {
 
 static apr_status_t destroy_config(void *dconfig) {
     evasive_config *cfg = (evasive_config *) dconfig;
-    ntt_destroy(cfg->hit_list);
-    free(cfg->email_notify);
-    free(cfg->log_dir);
-    free(cfg->system_command);
-    free(cfg);
+    if (cfg != NULL) {
+        ntt_destroy(cfg->hit_list);
+        free(cfg->email_notify);
+        free(cfg->log_dir);
+        free(cfg->system_command);
+        free(cfg);
+    }
     return APR_SUCCESS;
 }
 
