7e6d65929cc6b30dccdc8fd6810c3e8a2abf7695
Christian Göttsche <cgzones@googlemail.com>
2025-01-11 21:51:44 +0100

    Adjust log severity
    It's not an error to block a client.

--- mod_evasive24.c
+++ mod_evasive24.c
@@ -323,7 +323,7 @@ static int access_checker(request_rec *r)
 
     if (ret == cfg->http_reply
             && (ap_satisfies(r) != SATISFY_ANY || !ap_some_auth_required(r))) {
-        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+        ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r,
                 "client denied by server configuration: %s",
                 r->filename);
     }
